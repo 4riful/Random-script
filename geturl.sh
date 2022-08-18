a4fe8c218a9f714ecce990e130ac0ca1b5b14b3a
Ariful Anik <45453772+4riful@users.noreply.github.com>
2022-08-18 10:58:04 +0600

    update

--- geturl.sh
+++ geturl.sh
@@ -8,7 +8,7 @@ green="\e[32m"
 blue="\e[34m"
 cyan="\e[0;36m"
 end="\e[0m"
-#bannner 
+#bannner
 clear
 echo -e "\n\n"
 
@@ -21,9 +21,9 @@ printf "${cyan}TIP:Enter your Doamain list as first argument ,this script will f
 
 
 #WAYBACKURLS
-echo -e "\n\n\n[#]${blue}Using Waybackurls for gathering Urls for ${red}$1 .......${end}"| pv -qL $[20+(-2 + RANDOM%5)] 
+echo -e "\n\n\n[#]${blue}Using Waybackurls for gathering Urls for ${red}$1 .......${end}"| pv -qL $[20+(-2 + RANDOM%5)]
 cat "$1" | waybackurls | tee "$urlname".txt >/dev/null 2>&1;
-echo "Wayback Done for $urlname " | notify -silent >/dev/null 2>&1; 
+echo "Wayback Done for $urlname " | notify -silent >/dev/null 2>&1;
 printf "\n\n[#]${blue}URLS fetched using waybackurls & Stored in ${red}${bold}$urlname.txt${end}" | pv -qL $[10+(-2 + RANDOM%5)]
 
 #GAU
@@ -31,24 +31,29 @@ printf "\n\n[#]${blue}Using Gau for getting urls for${end}${red} $1 \n" | pv -qL
 cat "$1" | gau | tee gau.txt >/dev/null 2>&1;
 echo "Gau Done for $urlname " | notify -silent >/dev/null 2>&1;
 printf "\n\n[#]${blue}URLS fetched using gau & appended in ${red}${bold}$urlname.txt${end} \n\n" |  pv -qL $[15+(-2 + RANDOM%5)]
-# Sorting Out them 
+# Sorting Out them
 sort -u $urlname.txt gau.txt > fetchedurls.txt ;
 
 
 #filteration
 
-# cat $urlname.txt | anew gau.txt > filterd.txt>/dev/null 2>&1; 
+# cat $urlname.txt | anew gau.txt > filterd.txt>/dev/null 2>&1;
 # printf "\n\n[#]${red}Used ANEW for Removing Dupliacates" |  pv -qL $[10+(-2 + RANDOM%5)]
 # printf "\n\n[#]Sending results to URO for  filter out it again" | pv -qL $[16+(-2 + RANDOM%5)]
 # cat $urlname.txt | uro |tee fetchedurls.txt >/dev/null 2>&1;
 
 
-#removing tempfiles 
+#removing tempfiles
 
 printf "\n\n[#]${cyan}Removing Temporary files " | pv -qL $[10+(-2 + RANDOM%5)]
 rm gau.txt
 rm $urlname.txt
-echo -e "\n\n[#]${red}🎯Done :(" |  pv -qL $[10+(-2 + RANDOM%5)]
+echo -e "\n\n[#]${red}🎯Done :(${end}" |  pv -qL $[10+(-2 + RANDOM%5)]
+
+urlcount=$(cat fetchedurls.txt | wc -l)
+echo -e "\n\n[#]${cyan} ✔️ Total ${red}${urlcount} URls Fetched"
+
+
 echo -e "\n\n[#]📝Results are saved in ${red}fetchedurls.txt" | pv -qL $[10+(-2 + RANDOM%5)]
 echo -e "\n\n[#]Good luck 🤞" |  pv -qL $[10+(-2 + RANDOM%5)]
 echo "url fetched for $urlname " | notify -silent >/dev/null 2>&1;
