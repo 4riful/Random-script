e9a49b864cd9ed4adc7def0e5393841297269961
Ariful Anik <45453772+4riful@users.noreply.github.com>
2022-08-23 02:57:52 +0600

    whichhostfuncadded

--- sendme2dt.sh
+++ sendme2dt.sh
@@ -4,19 +4,24 @@
 
 #custom_config =~/.custom_path/config.yaml
 CONFIG=~/.config/notify/provider-config.yaml
-
+hname=$(hostname)
 function send2td {
     
 		if grep -q '^ telegram\|^telegram\|^    telegram' $CONFIG ; then
 			telegram_chat_id=$(cat ${CONFIG} | grep '^    telegram_chat_id\|^telegram_chat_id\|^    telegram_chat_id' | xargs | cut -d' ' -f2)
 			telegram_key=$(cat ${CONFIG} | grep '^    telegram_api_key\|^telegram_api_key\|^    telegram_apikey' | xargs | cut -d' ' -f2 )
-			curl -F document=@${1} "https://api.telegram.org/bot${telegram_key}/sendDocument?chat_id=${telegram_chat_id}" &>/dev/null
+			
+			curl -s -X POST https://api.telegram.org/bot$telegram_key/sendMessage -d chat_id=${telegram_chat_id} -d text="Sent from $hname" &>/dev/null &&
+			curl -F document=@${1} "https://api.telegram.org/bot${telegram_key}/sendDocument?chat_id=${telegram_chat_id}" &>/dev/null;
 		fi
 		if grep -q '^ discord\|^discord\|^    discord' $CONFIG ; then
 			discord_url=$(cat ${CONFIG} | grep '^ discord_webhook_url\|^discord_webhook_url\|^    discord_webhook_url' | xargs | cut -d' ' -f2)
-			curl -v -i -H "Accept: application/json" -H "Content-Type: multipart/form-data" -X POST -F file1=@${1} $discord_url &>/dev/null
+            curl -v -i -H "Accept: application/json" -H "Content-Type: multipart/form-data" -X POST -F 'payload_json={"username": "0xettaByte", "content": "'"Sent from ${hname}"'"}' \
+			-F file1=@${1} $discord_url &>/dev/null
 		fi
 	
 }
 send2td $1
-echo -e '\nSent Bro 😐'
+
+echo -e '\nSent Bro :D'
+
